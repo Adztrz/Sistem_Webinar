1b23cdb0ecd9f8026c9f7f77de6114773ba7a684
Aditia Nugraha <adztrz1@gmail.com>
2023-06-05 22:24:05 +0700

    Added Models

--- app/Models/Notifikasi.php
+++ app/Models/Notifikasi.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Notifikasi extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'id_notifikasi';
+    protected $fillable = ['event_id', 'namaEvent', 'tanggal', 'topikMateri', 'sertifikat'];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class, 'event_id', 'id_event');
+    }
 }
