49d6372f96bc5497895dc09bffec0a617c550767
bds16 <beatrixdatusarira@student.uns.ac.id>
2023-06-05 21:12:22 +0700

    Update 2023_05_15_151750_create_events_table.php

--- database/migrations/2023_05_15_151750_create_events_table.php
+++ database/migrations/2023_05_15_151750_create_events_table.php
@@ -34,7 +34,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function  down(): void
+    public function down(): void
     {
         Schema::dropIfExists('events');
     }
