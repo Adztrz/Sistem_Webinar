42104d412025c723d14a910a3a7eca9f384fcecb
Gita Bangun Prakoso <112094306+Bangsenjata@users.noreply.github.com>
2023-06-05 22:36:05 +0700

    add model pembicara and edit model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -41,4 +42,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function events(){
+        return $this->hasMany(events::class, 'user id', 'id_user');
+    }
+
+    public function pendaftaran(){
+        return $this->hasMany(pendaftaran::class, 'user_id', 'id_user');
+    }
 }
