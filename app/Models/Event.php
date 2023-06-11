7cddf0dba50b9291f9acbff0524d629277cc54bd
Aditia Nugraha <adztrz1@gmail.com>
2023-06-11 21:23:38 +0700

    Changed Event & User Models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id';
+    protected $fillable = 
+    [
+        'user_id', 
+        'eventDate', 
+        'eventLocation', 
+        'isPaid', 
+        'regisStartDate', 
+        'regisEndDate', 
+        'certificate', 
+        'certificateStartDate', 
+        'kategoriEvent'
+
+    ];
+
+
 }
