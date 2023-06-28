501b429b224e40846b4b1a174e25a58364fe911f
Gita Bangun Prakoso <112094306+Bangsenjata@users.noreply.github.com>
2023-06-28 20:18:26 +0700

    new controller

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function index()
+    {
+        return view('index');
+    }
+}
