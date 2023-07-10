2a80d54ccd07ee0560f8fd12069e00d7a34ae07c
Adztrz <adztrz@student.uns.ac.id>
2023-07-10 23:53:46 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,13 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'asal_instansi'
-    ];
-
+    protected $guarded =['id'];
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -40,10 +34,8 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
-
-    protected $primaryKey='id';
 
     protected static function newFactory()
     {
