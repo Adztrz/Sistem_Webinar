9dcece07241d5aeb1b82ed8ed96d39eaa054611a
Adztrz <adztrz@student.uns.ac.id>
2023-07-11 03:59:16 +0700

    Update event.blade.php

--- resources/views/dashboard/event.blade.php
+++ resources/views/dashboard/event.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('child')
-<div class="container-fluid mt-5">
+<div class="container-fluid mt-5" style="margin-bottom: 250px;">
   <div class="row">
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse margin-top: auto;">
       <div class="position-sticky pt-3">
@@ -24,17 +24,7 @@
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4" style="margin-bottom: 250px; margin-top: 50px;">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard User</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group me-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-          </div>
-          <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-            <span data-feather="calendar"></span>
-            This week
-          </button>
-        </div>
+        <div class="btn-toolbar mb-2 mb-md-0"></div>
       </div>
       <h4>Hai, {{ Auth::user()->role }} {{ Auth::user()->name }}</h4>
       <div class="table-responsive">
