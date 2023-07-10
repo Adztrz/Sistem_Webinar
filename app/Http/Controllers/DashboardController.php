7086faa14d87d722944bfbce88166a110ffbf88f
Bangsenjata <gpratama477@gmail.com>
2023-07-11 03:52:33 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Event;
-use App\Models\Registration;
 
 class DashboardController extends Controller
 {
